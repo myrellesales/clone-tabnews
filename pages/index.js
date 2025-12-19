597734309102f7d5e61e4910627ec4555b2ab7ac
Myrelle Sales <68798775+myrellesales@users.noreply.github.com>
2025-12-19 20:46:09 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     return <h1>Parabéns, Zé! Você foi escolhido para me fazer um PIX de Natal 🎄💸
-Obrigada pela colaboração.</h1>
+Obrigada pela colaboração!</h1>
 }
 
 export default Home;
